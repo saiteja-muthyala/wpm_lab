646ca454a9608ab6af387998c706142ac9bdf5eb
saiteja-muthyala <21eg107b58@anurag.edu.in>
2023-09-08 15:27:36 +0530

    commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,17 @@
-/* GET 'home' page */
 const homelist = (req, res) => {
-    res.render('index', { title: 'Home' });
-   };
-   /* GET 'Location info' page */
-   const locationInfo = (req, res) => {
-    res.render('index', { title: 'Location info' });
-   };
-   /* GET 'Add review' page */
-   const addReview = (req, res) => {
-    res.render('index', { title: 'Add review' });
-   };
-   module.exports = {
+    res.render('locations-list', { title: 'Home' });
+  };
+  
+  const locationInfo = (req, res) => {
+    res.render('location-info', { title: 'Location Info' });
+  };
+  
+  const addReview = (req, res) => {
+    res.render('location-review-form', { title: 'Add review' });
+  };
+  
+  module.exports = {
     homelist,
     locationInfo,
     addReview
-   };
+  };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
-const ctrlMain = require('../controllers/main');
-router.get('/',ctrlMain.index);
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
